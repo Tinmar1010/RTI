ec488add0550fd0abe125032494ef367556573e8
Bodanor <christos.papadopoulos@student.hepl.be>
2023-12-07 15:06:46 -0500

    Various improvement to functions

--- src/Client_Achat/protocol/Libsocket.java
+++ src/Client_Achat/protocol/Libsocket.java
@@ -10,12 +10,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Libsocket{
-    private final Socket client_socket;
     private final OutputStream dos;
     private final InputStream dis;
 
     public Libsocket(String target_ip, int target_port) throws IOException {
-        client_socket = new Socket(target_ip, target_port);
+        Socket client_socket = new Socket(target_ip, target_port);
         dos = client_socket.getOutputStream();
         dis = client_socket.getInputStream();
     }
@@ -28,18 +27,17 @@ public class Libsocket{
         for (int i = 0; i < data.length(); i += 1019)
             data_body_packets.add(data.substring(i, Math.min(data.length(), i + 1019)));
 
-        for (int i = 0; i < data_body_packets.size(); i++) {
-            if (data_body_packets.get(i).length() >= 1019) {
-                packet.put((byte)1);
+        for (String dataBodyPacket : data_body_packets) {
+            if (dataBodyPacket.length() >= 1019) {
+                packet.put((byte) 1);
                 packet.putInt(1019);
-                packet.put(data_body_packets.get(i).getBytes());
+                packet.put(dataBodyPacket.getBytes());
+            } else {
+                packet.put((byte) 0);
+                packet.putInt(dataBodyPacket.length());
+                packet.put(dataBodyPacket.getBytes());
             }
-            else {
-                packet.put((byte)0);
-                packet.putInt(data_body_packets.get(i).length());
-                packet.put(data_body_packets.get(i).getBytes());
-            }
-            dos.write(packet.array(), 0, data_body_packets.get(i).length() + 5);
+            dos.write(packet.array(), 0, dataBodyPacket.length() + 5);
             packet.reset();
         }
         return 0;
@@ -49,16 +47,16 @@ public class Libsocket{
     public String Receive_msg() throws IOException {
         byte has_next_packet = 0;
         byte[] header;
-        String message = "";
+        StringBuilder message = new StringBuilder();
 
         do {
             header = dis.readNBytes(5);
             has_next_packet = header[0];
             String current_message = new String(dis.readNBytes(header[1]));
-            message = message + current_message;
+            message.append(current_message);
 
         }while (has_next_packet == 1);
 
-        return message;
+        return message.toString();
     }
 }
--- src/Client_Achat/protocol/OVESP.java
+++ src/Client_Achat/protocol/OVESP.java
@@ -2,38 +2,53 @@ package Client_Achat.protocol;
 
 import java.io.IOException;
 import java.net.SocketException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class OVESP {
         private final Libsocket serverConnection;
+
     public OVESP(String ip, int port) throws IOException{
         this.serverConnection = new Libsocket(ip, port);
     }
     public int OVESPLogin(String username, String Password, boolean newUserFlag) throws IOException {
-        serverConnection.SendMsg("LOGIN#" + username + "#" + Password + "#" + (newUserFlag ? "1": "0") + "#");
+        ArrayList<String> request = new ArrayList<String>();
+        request.add("LOGIN");
+        request.add(username);
+        request.add(Password);
+        request.add((newUserFlag ? "1" : "0"));
+
+        serverConnection.SendMsg(createRequest(request));
         String[] response = serverConnection.Receive_msg().split("#");
 
         if (response[0].equals("LOGIN")) {
-            if (response[1].trim().equals("BAD_REQUEST"))
-                return -3;
-            else if (response[1].trim().equals("OK"))
-                return 0;
-            else if (response[1].trim().equals("KO")) {
-                if (response[2].trim().equals("BAD_USER"))
-                    return 1;
-                else if (response[2].trim().equals("BAD_PASS"))
-                    return 2;
-                else if (response[2].trim().equals("DB_FAIL"))
-                    return 3;
-                else if (response[2].trim().equals("ALREADY_EXISTS"))
-                    return 4;
-                else
-                    return -4;
+            switch (response[1].trim()) {
+                case "BAD_REQUEST" -> {
+                    return -3;
+                }
+                case "OK" -> {
+                    return 0;
+                }
+                case "KO" -> {
+                    return switch (response[2].trim()) {
+                        case "BAD_USER" -> 1;
+                        case "BAD_PASS" -> 2;
+                        case "DB_FAIL" -> 3;
+                        case "ALREADY_EXISTS" -> 4;
+                        default -> -4;
+                    };
+                }
             }
         }
         else
             return -4;
 
         return 0;
+    }
+    private String createRequest(ArrayList<String> tokens) {
+        StringBuilder request = new StringBuilder();
+        for (String s: tokens) {
+            request.append(s).append("#");
+        }
+        return request.toString();
     }
 }
